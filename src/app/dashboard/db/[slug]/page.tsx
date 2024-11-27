1e670b994875891040658bf621057b162b5ff8fc
GweepCreative <gweepcreative@gmail.com>
2024-11-27 21:30:58 +0300

    data table

--- src/app/dashboard/db/[slug]/page.tsx
+++ src/app/dashboard/db/[slug]/page.tsx
@@ -1,7 +1,25 @@
-"use client";
-import { useParams } from "next/navigation";
+import { Payment, columns } from "./columns"
+import { DataTable } from "./data-table"
 
-export default function Page() {
-  const { slug } = useParams();
-  return <h1>{slug}</h1>;
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ]
+}
+
+export default async function Page() {
+  const data = await getData()
+
+  return (
+    <div className="w-full  mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  )
 }
